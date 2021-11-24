c67674a984067a2a227fdef87959eacfecf7add5
mohammad-irvan <mohirvan66@gmail.com>
2021-11-24 23:48:07 +0800

    clone trello

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tutorial dari elfin sanjaya [https://www.youtube.com/watch?v=ZDa4lk1DlI0]
+
+## Cara Menjalank Aplikasinya
+
+* 1.npm install
+* 2.npm start atau DEBUG=clone-trello:* npm start
+
+# API TOOlS
+* INSOMNIA
---
+++ app/config/database.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: 'pmauser',
+    password: null,
+    database: 'clone_trello',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'pmauser',
+    password: null,
+    database: 'clone_trello',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'pmauser',
+    password: null,
+    database: 'clone_trello',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    
+  },
+};
